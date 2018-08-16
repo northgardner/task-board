8051406342de68e86048b23442032ef5b99245d4
Jonathan Greenberg <northgardner@users.noreply.github.com>
2018-08-16 15:30:22 -0400

    Delete xframes.js

--- xframes.js
+++
@@ -1,3 +0,0 @@
- // Serve HTML with no X-Frame-Options header (in Apps Script server-side code).
- var output = HtmlService.createHtmlOutput('<b>Hello, world!</b>');
- output.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
